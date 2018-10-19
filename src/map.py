2b1e4d69a7a6c1b84f8ed5fbfca6003dfe98c509
MidnightDreary <janikdriehaus@gmx.co.uk>
2018-10-19 16:09:34 +0200

    Update map.py
    Update copyright

--- src/map.py
+++ src/map.py
@@ -1,3 +1,4 @@
+# Copyright (C) 2018  MidnightDreary/Janik Driehaus <jandriehaus@gmail.com>
 # Copyright (C) 2018  Arc676/Alessandro Vinciguerra <alesvinciguerra@gmail.com>
 
 # This program is free software: you can redistribute it and/or modify
